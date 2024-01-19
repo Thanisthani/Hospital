58b26fce861b2a617dbc1c7e174bb92a6a61f935
ThaniThanis <thanithanis171198@gmail.com>
2024-01-19 16:31:20 +0530

    Build doctor list screen

--- src/components/DoctorList/index.jsx
+++ src/components/DoctorList/index.jsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { doctorList } from '../../constants'
+import { styles } from './style'
 
 export default function DoctorList() {
   return (
     <View>
-      <Text>DoctorListScreen</Text>
+      <FlatList
+        data={doctorList}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <View style={styles.imgWrapper}>
+              <Image source={{ uri: item.imageUrl }} style={styles.pic} />
+              <View>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text style={styles.categoryTxt}>{item.category}</Text>
+                <Text style={styles.expTxt}>{item.Experiance} Years</Text>
+              </View>
+            </View>
+            <TouchableOpacity style={styles.btn}>
+              <Text style={styles.btnTxt}>Book Appointment</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   )
 }
--- src/components/DoctorList/style.jsx
+++ src/components/DoctorList/style.jsx
@@ -4,5 +4,53 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen'
+import { colorSheet } from '../../ColorSheet'
 
-const styles = StyleSheet.create({})
+export const styles = StyleSheet.create({
+  pic: {
+    width: wp(32),
+    height: hp(18),
+    borderRadius: 15,
+  },
+  details: {},
+  card: {
+    backgroundColor: colorSheet.white,
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: hp(1.5),
+    width: wp(90),
+  },
+  imgWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+  },
+  name: {
+    fontSize: RFValue(16),
+    fontFamily: 'Inter_700Bold',
+  },
+  categoryTxt: {
+    fontSize: RFValue(12),
+    fontFamily: 'Inter_400Regular',
+    color: colorSheet.secondary1,
+  },
+  expTxt: {
+    fontSize: RFValue(12),
+    fontFamily: 'Inter_700Bold',
+    color: colorSheet.primary2,
+  },
+  btn: {
+    width: '100%',
+    paddingVertical: hp(1),
+    alignItems: 'center',
+    backgroundColor: colorSheet.secondary4,
+    borderRadius: 10,
+    marginTop: hp(1.5),
+  },
+  btnTxt: {
+    color: colorSheet.secondary3,
+    fontFamily: 'Inter_600SemiBold',
+    fontSize: RFValue(13),
+  },
+})
